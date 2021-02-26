4b57cc1d624fb50160da0f57f135d49c6f4eb855
Obito1903 <sam200133@gmail.com>
2021-02-26 12:13:51 +0000

    first commit

---
+++ add_user.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+docker stop filemanager
+
+filebrowser users add $1 $2 --scope /srv/$1 --locale fr
+
+docker start filemanager
+
+docker run -d --restart unless-stopped --name $1 --net v1_free_real_estate -v $(pwd)/users/data/$1:/var/www/html:rw trafex/alpine-nginx-php7
+docker run -d \
+  --net v1_free_real_estate \
+  --name=code-$1 \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=Europe/Paris \
+  -e PASSWORD=$2 \
+  -v /home/pog/docker/test/v1/users/data/$1:/config/workspace \
+  --restart unless-stopped \
+  ghcr.io/linuxserver/code-server
