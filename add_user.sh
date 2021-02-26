9a565bb9a016ea83017b71d899ce1014f62cc451
Obito1903 <sam200133@gmail.com>
2021-02-26 13:08:55 +0000

    fixed fex things in add_user.sh

--- add_user.sh
+++ add_user.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker stop filemanager
+docker stop files
 
-filebrowser users add $1 $2 --scope /srv/$1 --locale fr
+filebrowser users add $1 $2 --scope /srv/$1 --locale fr --perm
 
-docker start filemanager
+docker start files
 
 docker run -d --restart unless-stopped --name $1 --net v1_free_real_estate -v $(pwd)/users/data/$1:/var/www/html:rw trafex/alpine-nginx-php7
 docker run -d \
