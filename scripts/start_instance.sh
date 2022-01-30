f073bf07f727dcfa6359d61e5da6f86c91c1ca15
obito1903 <sam200133@gmail.com>
2022-01-30 20:12:45 +0100

    nginx config gneu gneu

--- scripts/start_instance.sh
+++ scripts/start_instance.sh
@@ -13,10 +13,10 @@ docker run \
   -v $(pwd)/../data/users/$1/.code:/config \
   -v $(pwd)/../data/users/$1/site:/config/workspace \
   --network fre-ng_fre \
-  --label traefik.http.routers.$1-code.rule=Host\(\`$1-code.${DOMAIN}\`\) \
+  --label traefik.http.routers.$1-code.rule=Host\(\`$1-code.${DOMAIN}.${TLD}\`\) \
   --label traefik.http.services.$1-code.loadbalancer.server.port=8443 \
   --label traefik.http.routers.$1-code.service=$1-code \
-  --label traefik.http.routers.$1.rule=Host\(\`$1.${DOMAIN}\`\) \
+  --label traefik.http.routers.$1.rule=Host\(\`$1.${DOMAIN}.${TLD}\`\) \
   --label traefik.http.services.$1.loadbalancer.server.port=80 \
   --label traefik.http.routers.$1.service=$1 \
   --rm docker.io/fre/code-php
