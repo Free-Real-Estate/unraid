16f86128fbc7fcff92c445602b180e0c06337d9b
FRE <alexandru.nicolae.moldovan@eisti.fr>
2022-02-02 00:44:49 +0000

    ssh

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -7,5 +7,8 @@ iptables -I DOCKER-USER -s 192.168.1.0/24 -j DROP
 iptables -I DOCKER-USER -s 192.168.1.1 -j ACCEPT
 iptables -I DOCKER-USER -p tcp -s 192.168.1.1 --sport 80 --j DROP
 # allow unraid proxy
-iptables -I DOCKER-USER -s 192.168.1.33 -j ACCEPT
+iptables -I DOCKER-USER -s 192.168.1.30 -j ACCEPT
 iptables -I DOCKER-USER -p tcp -s 192.168.1.30 ! --dport 80 --j DROP
+# disallow containers to ssh host
+iptables -A INPUT -p tcp --dport 22 --source 192.168.1.0/24 -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j DROP
